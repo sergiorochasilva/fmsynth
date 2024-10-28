dbb71efc81cc6d48152b78171785eb63203f70e9
Sergio Silva <sergiosilva@nasajon.com.br>
2024-10-28 08:37:41 -0300

    Primeiro modelo treinavel

--- fm_synth.py
+++ fm_synth.py
@@ -22,6 +22,12 @@ class FMSynth:
         beta3: float,
         amplitude3: float,
         frequency3: float,
+        beta4: float,
+        amplitude4: float,
+        frequency4: float,
+        beta5: float,
+        amplitude5: float,
+        frequency5: float,
         beta_carrier: float,
         amplitude_carrier: float,
         attack: float,
@@ -37,6 +43,12 @@ class FMSynth:
         self.beta3 = beta3
         self.amplitude3 = amplitude3
         self.frequency3 = frequency3
+        self.beta4 = beta4
+        self.amplitude4 = amplitude4
+        self.frequency4 = frequency4
+        self.beta5 = beta5
+        self.amplitude5 = amplitude5
+        self.frequency5 = frequency5
         self.beta_carrier = beta_carrier
         self.amplitude_carrier = amplitude_carrier
         self.attack = attack
@@ -70,16 +82,36 @@ class FMSynth:
             self.amplitude3,
             self.frequency3 * frequency_carrier,
         )
+        modulator4 = self._synth_operator(
+            self.beta4,
+            modulator3,
+            audio_seconds,
+            self.amplitude4,
+            self.frequency4 * frequency_carrier,
+        )
+        modulator5 = self._synth_operator(
+            self.beta5,
+            modulator4,
+            audio_seconds,
+            self.amplitude5,
+            self.frequency5 * frequency_carrier,
+        )
         carrier = self._synth_operator(
             self.beta_carrier,
-            modulator3,
+            modulator5,
             audio_seconds,
             self.amplitude_carrier,
             frequency_carrier,
         )
 
         return self._adsr(
-            audio_seconds, self.attack, self.decay, self.sustain, self.release, carrier
+            len(carrier),
+            audio_seconds,
+            self.attack,
+            self.decay,
+            self.sustain,
+            self.release,
+            carrier,
         )
 
     def _synth_operator(
@@ -116,15 +148,14 @@ class FMSynth:
 
     def _adsr(
         self,
-        audio_seconds: int,
+        sample_length: int,
+        audio_seconds: float,
         attack: float,
         decay: float,
         sustain: float,
         release: float,
         signal: np.ndarray,
     ):
-        total_samples = SAMPLE_RATE * audio_seconds
-
         # Attack
         attack_samples_qtd = int(SAMPLE_RATE * attack)
         attack_coef = 1.0 / attack_samples_qtd
@@ -143,14 +174,8 @@ class FMSynth:
         release_samples = np.arange(sustain, 0, release_coef)
 
         # Sustain
-        sustain_samples_qtd = int(
-            SAMPLE_RATE * (audio_seconds - attack - decay - release)
-        )
-        sustain_samples_qtd += total_samples - (
-            attack_samples_qtd
-            + decay_samples_qtd
-            + sustain_samples_qtd
-            + release_samples_qtd
+        sustain_samples_qtd = sample_length - (
+            attack_samples.shape[0] + decay_samples.shape[0] + release_samples.shape[0]
         )
         sustain_samples = np.ones((sustain_samples_qtd))
         sustain_samples *= sustain
@@ -167,21 +192,50 @@ class FMSynth:
 
 
 if __name__ == "__main__":
-    fm_synth = FMSynth(
-        amplitude1=1.0,
-        frequency1=2.2,
-        beta2=0.0,
-        amplitude2=1,
-        frequency2=2.0,
-        beta3=0.3,
-        amplitude3=1,
-        frequency3=1.5,
-        beta_carrier=2.5,
-        amplitude_carrier=0.8,
-        attack=0.01,
-        decay=0.2,
-        sustain=0.2,
-        release=0.3,
-    )
-    signal = fm_synth.synth_alg1(3, 440.0)
+    # fm_synth = FMSynth(
+    #     amplitude1=1.0,
+    #     frequency1=2.2,
+    #     beta2=0.0,
+    #     amplitude2=1,
+    #     frequency2=2.0,
+    #     beta3=0.3,
+    #     amplitude3=1,
+    #     frequency3=1.5,
+    #     beta4=0.3,
+    #     amplitude4=1,
+    #     frequency4=1.5,
+    #     beta5=0.3,
+    #     amplitude5=1,
+    #     frequency5=1.5,
+    #     beta_carrier=2.5,
+    #     amplitude_carrier=0.8,
+    #     attack=0.01,
+    #     decay=0.2,
+    #     sustain=0.2,
+    #     release=0.3,
+    # )
+    data = {
+        "amplitude1": 0.396,
+        "frequency1": 0.07,
+        "beta2": 0.273,
+        "amplitude2": 0.672,
+        "frequency2": 0.133,
+        "beta3": 0.278,
+        "amplitude3": 0.692,
+        "frequency3": 0.135,
+        "beta4": 0.293,
+        "amplitude4": 0.083,
+        "frequency4": 0.598,
+        "beta5": 0.945,
+        "amplitude5": 0.861,
+        "frequency5": 0.13,
+        "beta_carrier": 0.742,
+        "amplitude_carrier": 0.93,
+        "attack": 0.72,
+        "decay": 0.865,
+        "sustain": 0.545,
+        "release": 0.453,
+    }
+    fm_synth = FMSynth(**data)
+    signal = fm_synth.synth_alg1(4, 18618.554)
     sf.write("output.wav", signal, SAMPLE_RATE)
