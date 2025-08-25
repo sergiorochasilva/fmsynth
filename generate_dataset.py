de79e53d392ff020ce07b150a763b599f9902449
Sergio Silva <sergiosilva@nasajon.com.br>
2025-08-24 21:13:16 -0300

    Primeira versão convergindo

--- generate_dataset.py
+++ generate_dataset.py
@@ -9,14 +9,15 @@ import pandas as pd
 from fm_synth import FMSynth, SAMPLE_RATE
 
 duracao_amostras = 1
-tamanho_dataset = 5000
+tamanho_dataset = 20000
 precisao_decimal = 3
 
-with open("dataset/parameters.json", "w") as f:
+with open("dataset_big/parameters.json", "w") as f:
     f.write("[\n")
     for i in range(tamanho_dataset):
+        print(f"Gerando amostra {i + 1} de {tamanho_dataset}...")
         # Sorteando uma frequencia báse, na faixa audível humana
-        frequencia_base = random.random() * 20000 + 20
+        frequencia_base = random.random() * 2000 + 2000
         frequencia_base = round(frequencia_base, precisao_decimal)
 
         # Sorteando os demais parâmetros da síntese
@@ -105,6 +106,6 @@ with open("dataset/parameters.json", "w") as f:
         signal = fm_synth.synth_alg1(duracao_amostras, frequencia_base)
 
         # Escrevendo o áudio em arquivo
-        sf.write(f"dataset/sample_{i}.wav", signal, SAMPLE_RATE)
+        sf.write(f"dataset_big/sample_{i}.wav", signal, SAMPLE_RATE)
 
     f.write("]")
